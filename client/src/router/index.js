f5a678e35b1064e469051d977a053251c957b1f2
Machi0 <jamaalsa@gmail.com>
2020-02-08 17:51:18 -0500

    Redirects if trying to visit api routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,6 +14,10 @@ const routes = [
     component: Home,
   },
   {
+    path: '/api*',
+    redirect: { name: 'home' },
+  },
+  {
     path: '/unib/combos',
     name: 'unib combos',
     component: UnibCombos,
